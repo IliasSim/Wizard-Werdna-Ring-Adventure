a84d05cef07a6482b3199853566696a72761a7d2
IliasSim <iliassimky@gmail.com>
2021-10-14 17:34:24 +0300

    Update GameSettings.py

--- GameSettings.py
+++ GameSettings.py
@@ -1,10 +1,12 @@
-'''The GameSettings is an singleton class which is used for the storage of parameters that many classes of the game have to have access.'''
-import os
-from Tile import Tile
-xtile = 80
-ytile = 40
-mapWidth = 960
-mapHeigth = 480
+'''The GameSettings is a module  which is used for the storage of parameters that many classes of the game have to have access.'''
+
+screenFactor = 3
+reward = 0
+gameCount = 0
+mapWidth = 0
+mapHeigth = 0
+xtile = 0
+ytile = 0
 startX = 0
 startY = 0
 radius = 6
@@ -13,11 +15,7 @@ exitx = 0
 exity = 0
 enemies = []
 game_text = []
-
-tiles = [[0]*ytile for i in range(xtile)]
-for y in range(ytile):
-    for x in range(xtile):
-        tiles[x][y] = Tile(x,y)
+tiles = []
 
 def addGameText(text):
     '''Creates a list with the text to be depicted at the game'''
