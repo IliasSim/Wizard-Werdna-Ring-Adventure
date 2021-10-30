8156e93147dbfbce3c7ec1590564797e9553b33e
IliasSim <iliassimky@gmail.com>
2021-10-30 11:35:39 +0300

    Update playerActor2CNNn_steps.py

--- playerActor2CNNn_steps.py
+++ playerActor2CNNn_steps.py
@@ -1,6 +1,8 @@
 from GamePAI import GamePAI
 import pygame
 import numpy as np
+#import os
+#os.environ["CUDA_VISIBLE_DEVICES"]="-1" 
 import tensorflow as tf
 import tensorflow_probability as tfp
 import pandas as pd
@@ -10,10 +12,13 @@ import sys
 
 featuresCNN1 = 16
 CNN1Shape = 4
-CNN1Step = 2
+CNN1Step = 4
 featuresCNN2 = 32
 CNN2Shape = 2
 CNN2Step = 2
+featuresCNN3 = 64
+CNN3Shape = 5
+CNN3Step = 5
 denseLayerN = 512
 denseLayerNL_2 = 32
 denseLayerNL_3= 64
@@ -23,44 +28,51 @@ dropoutRate1 = 0.3
 dropoutRate2 = 0.3
 n_step = 4
 input = (148,148,3)
+physical_devices = tf.config.list_physical_devices('GPU')
+try:
+    tf.config.experimental.set_memory_growth(physical_devices[0], True)
+except:
+    pass
 class critic(tf.keras.Model):
     '''This class creates the model of the critic part of the reiforcment learning algorithm'''
     def __init__(self):
         super().__init__()
         # self.l1 = tf.keras.layers.Conv2D(32,(8,8),(4,4),activation = 'relu',input_shape=(148,148,3))
-        # self.l2 = tf.keras.layers.Conv2D(64,(4,4),2,activation = 'relu')
         # self.l1 = tf.keras.layers.Conv2D(64,(4,4),(1,1),activation = 'relu',input_shape=(148,148,3))
-        #self.l1 = tf.keras.layers.Conv2D(featuresCNN1,(CNN1Shape,CNN1Shape),(CNN1Step,CNN1Step),activation = 'relu',input_shape = input)
-        #self.l2 = tf.keras.layers.Conv2D(featuresCNN2,(CNN2Shape,CNN2Shape),(CNN2Step,CNN2Step),activation = 'relu')
-        #self.l3 = tf.keras.layers.Flatten()
-        #self.l4 = tf.keras.layers.Dense(denseLayerN,activation = 'relu')
-        #self.v = tf.keras.layers.Dense(1, activation = None)
-        self.l1 = tf.keras.layers.Conv2D(featuresCNN1,(CNN1Shape,CNN1Shape),(CNN1Step,CNN1Step),activation = 'relu')
-        self.l2 = tf.keras.layers.Conv2D(featuresCNN2,(CNN2Shape,CNN2Shape),(CNN2Step,CNN2Step),activation = 'relu')
-        self.l3 = tf.keras.layers.Flatten()
-        self.l4 = tf.keras.layers.Dense(denseLayerN,activation = 'relu')
-        self.l1_2 = tf.keras.layers.Dense(denseLayerNL_2,activation = 'relu')
-        self.l1_21 = tf.keras.layers.Dense(denseLayerNL_21,activation = 'relu')
-        self.l1_3 = tf.keras.layers.Dense(denseLayerNL_3,activation = 'relu')
-        self.l1_31 = tf.keras.layers.Dense(denseLayerNL_31,activation = 'relu')
+        #self.l1 = tf.keras.layers.ConvLSTM2D(featuresCNN1,(CNN1Shape,CNN1Shape),(CNN1Step,CNN1Step),return_sequences = True,data_format='channels_last')
+        #self.l2 = tf.keras.layers.ConvLSTM2D(featuresCNN2,(CNN2Shape,CNN2Shape),(CNN2Step,CNN2Step),return_sequences = True,data_format='channels_last')
+        # self.l3 = tf.keras.layers.Flatten()
+        # self.l4 = tf.keras.layers.Dense(denseLayerN,activation = 'relu')
+        # self.a = tf.keras.layers.Dense(9, activation ='softmax')
+        self.l1 = tf.keras.layers.Conv3D(featuresCNN1,(1,CNN1Shape,CNN1Shape),(1,CNN1Step,CNN1Step),activation = 'relu')
+        self.l2 = tf.keras.layers.Conv3D(featuresCNN2,(1,CNN2Shape,CNN2Shape),(1,CNN2Step,CNN2Step),activation = 'relu')
+        #self.l3 = tf.keras.layers.Conv3D(featuresCNN3,(1,CNN3Shape,CNN3Shape),(1,CNN3Step,CNN3Step),activation = 'relu')
+        self.l4 = tf.keras.layers.Reshape((8,10368))
+        self.l5 = tf.keras.layers.LSTM(denseLayerN)
+        self.l6 = tf.keras.layers.Dense(denseLayerNL_2,activation = 'relu')
+        self.l7 = tf.keras.layers.LSTM(denseLayerNL_21)
+        self.l8 = tf.keras.layers.Dense(denseLayerNL_3,activation = 'relu')
+        self.l9 = tf.keras.layers.LSTM(denseLayerNL_31)
         self.conc1 = tf.keras.layers.Concatenate(axis=-1)
         self.conc2 = tf.keras.layers.Concatenate(axis=-1)
         #self.drop1 = tf.keras.layers.Dropout(dropoutRate1)
         #self.drop2 = tf.keras.layers.Dropout(dropoutRate2)
-        self.v = tf.keras.layers.Dense(1, activation = None)
-        
+        #self.conc = tf.keras.layers.concatenate([self.l4,self.l1_2],axis = -1)
+        #self.conc3 = tf.keras.layers.concatenate([self.l1_3,self.conc],axis = -1)
+        self.v = tf.keras.layers.Dense(1, activation =None)
+      
 
     def call(self,input_data,input_data1,input_data2):
         x = self.l1(input_data)
         x = self.l2(x)
-        x = self.l3(x)
-        print(x.shape)
-        x= self.l4(x)
-        y = self.l1_2(input_data1)
-        #y = self.l1_21(y) 
+        #x= self.l3(x)
+        x = self.l4(x)
+        x= self.l5(x)
+        y = self.l6(input_data1)
+        y = self.l7(y)
         #y = self.drop1(y)
-        z = self.l1_3(input_data2)
-        #z = self.l1_31(z)
+        z = self.l8(input_data2)
+        z = self.l9(z)
         #z = self.drop2(z)
         h = self.conc1((y,z))
         #e = self.add((y,d))
@@ -74,19 +86,20 @@ class actor(tf.keras.Model):
         super().__init__()
         # self.l1 = tf.keras.layers.Conv2D(32,(8,8),(4,4),activation = 'relu',input_shape=(148,148,3))
         # self.l1 = tf.keras.layers.Conv2D(64,(4,4),(1,1),activation = 'relu',input_shape=(148,148,3))
-        # self.l1 = tf.keras.layers.Conv2D(featuresCNN1,(CNN1Shape,CNN1Shape),(CNN1Step,CNN1Step),activation = 'relu',input_shape=input)
-        # self.l2 = tf.keras.layers.Conv2D(featuresCNN2,(CNN2Shape,CNN2Shape),(CNN2Step,CNN2Step),activation = 'relu')
+        #self.l1 = tf.keras.layers.ConvLSTM2D(featuresCNN1,(CNN1Shape,CNN1Shape),(CNN1Step,CNN1Step),return_sequences = True,data_format='channels_last')
+        #self.l2 = tf.keras.layers.ConvLSTM2D(featuresCNN2,(CNN2Shape,CNN2Shape),(CNN2Step,CNN2Step),return_sequences = True,data_format='channels_last')
         # self.l3 = tf.keras.layers.Flatten()
         # self.l4 = tf.keras.layers.Dense(denseLayerN,activation = 'relu')
         # self.a = tf.keras.layers.Dense(9, activation ='softmax')
-        self.l1 = tf.keras.layers.Conv2D(featuresCNN1,(CNN1Shape,CNN1Shape),(CNN1Step,CNN1Step),activation = 'relu')
-        self.l2 = tf.keras.layers.Conv2D(featuresCNN2,(CNN2Shape,CNN2Shape),(CNN2Step,CNN2Step),activation = 'relu')
-        self.l3 = tf.keras.layers.Flatten()
-        self.l4 = tf.keras.layers.Dense(denseLayerN,activation = 'relu')
-        self.l1_2 = tf.keras.layers.Dense(denseLayerNL_2,activation = 'relu')
-        self.l1_21 = tf.keras.layers.Dense(denseLayerNL_21,activation = 'relu')
-        self.l1_3 = tf.keras.layers.Dense(denseLayerNL_3,activation = 'relu')
-        self.l1_31 = tf.keras.layers.Dense(denseLayerNL_31,activation = 'relu')
+        self.l1 = tf.keras.layers.Conv3D(featuresCNN1,(1,CNN1Shape,CNN1Shape),(1,CNN1Step,CNN1Step),activation = 'relu')
+        self.l2 = tf.keras.layers.Conv3D(featuresCNN2,(1,CNN2Shape,CNN2Shape),(1,CNN2Step,CNN2Step),activation = 'relu')
+        #self.l3 = tf.keras.layers.Conv3D(featuresCNN3,(1,CNN3Shape,CNN3Shape),(1,CNN3Step,CNN3Step),activation = 'relu')
+        self.l4 = tf.keras.layers.Reshape((8,10368))
+        self.l5 = tf.keras.layers.LSTM(denseLayerN)
+        self.l6 = tf.keras.layers.Dense(denseLayerNL_2,activation = 'relu')
+        self.l7 = tf.keras.layers.LSTM(denseLayerNL_21)
+        self.l8 = tf.keras.layers.Dense(denseLayerNL_3,activation = 'relu')
+        self.l9 = tf.keras.layers.LSTM(denseLayerNL_31)
         self.conc1 = tf.keras.layers.Concatenate(axis=-1)
         self.conc2 = tf.keras.layers.Concatenate(axis=-1)
         #self.drop1 = tf.keras.layers.Dropout(dropoutRate1)
@@ -99,13 +112,14 @@ class actor(tf.keras.Model):
     def call(self,input_data,input_data1,input_data2):
         x = self.l1(input_data)
         x = self.l2(x)
-        x = self.l3(x)
-        x= self.l4(x)
-        y = self.l1_2(input_data1)
-        y = self.l1_21(y)
+        #x= self.l3(x)
+        x = self.l4(x)
+        x= self.l5(x)
+        y = self.l6(input_data1)
+        y = self.l7(y)
         #y = self.drop1(y)
-        z = self.l1_3(input_data2)
-        z = self.l1_31(z)
+        z = self.l8(input_data2)
+        z = self.l9(z)
         #z = self.drop2(z)
         h = self.conc1((y,z))
         #e = self.add((y,d))
@@ -146,6 +160,7 @@ class agent():
             discnt_rewards.append(sum_reward)
         discnt_rewards.reverse()
         states = np.array(states, dtype=np.float32)
+        states =  np.squeeze(states,axis = 1)
         playerstatus = np.array(playerstatus,dtype=np.int32)
         playerstatus = np.squeeze(playerstatus)
         gameTexts = np.array(gameTexts,dtype=np.int32)
@@ -155,7 +170,7 @@ class agent():
         gameTexts = np.squeeze(gameTexts,axis = 1)
         #print(gameTexts.shape,gameTexts,playerstatus,playerstatus.shape)
         #gameTexts = np.expand_dims(gameTexts, axis=0)
-        #print(gameTexts.shape,gameTexts,playerstatus,playerstatus.shape,states.shape)
+        #print(gameTexts.shape,playerstatus.shape,states.shape)
         actions = np.array(actions, dtype=np.int32)
         discnt_rewards = np.array(discnt_rewards, dtype=np.float32)
         return states,playerstatus,gameTexts, actions, discnt_rewards
@@ -239,9 +254,14 @@ for s in range(episode):
                 sys.exit()
         steps += 1
         action = agentoo7.act(state,playerStatus,gameText)
-        next_state,reward, next_playerStatus, next_gameText  = game.playerAction(action)
+        #done = game.gameOver(s)
+        next_state,reward, next_playerStatus, next_gameText,done  = game.playerAction(action)
+        action_name = {0:'up',1:'right',2:'down',3:'left',4:'rest',5:'hp',6:'mp',7:'attack',8:'pick'}
+        print(action_name[action],reward,game.cave)
+        if done:
+            game.__init__(1,'Connan',444,444,1,True,s,False)
         #next_gameText = agentoo7.preprocess(next_gameText)
-        done = game.gameOver(s)
+        #done = game.gameOver(s)
         rewards.append(reward)
         states.append(state)
         playerstatus.append(playerStatus)
@@ -252,13 +272,11 @@ for s in range(episode):
         playerStatus = next_playerStatus
         gameText = next_gameText
         total_reward += reward
-        action_name = {0:'up',1:'right',2:'down',3:'left',4:'rest',5:'hp',6:'mp',7:'attack',8:'pick'}
-        print(action_name[action],reward,game.cave)
         if steps%1000 == 0:
             print(steps,total_reward,action_name[action],game.cave)
 
         if s <= 2000: 
-            if steps >= 5000 and game.cave < 2:
+            if steps >= 500 and game.cave < 2:
                 noVideo = True
                 if s% 100 == 0:
                     game.__init__(1,'Connan',444,444,1,True,s,False)
@@ -266,7 +284,7 @@ for s in range(episode):
                 if noVideo:
                     game.__init__(1,'Connan',444,444,1,True,s,False)
                 gc.collect()
-                #print(s,total_reward,game.cave)
+                print(s,total_reward,game.cave)
                 done = True
         if s > 2000: 
             if steps >= 10000 and game.cave < 2:
@@ -277,7 +295,7 @@ for s in range(episode):
                 if noVideo:
                     game.__init__(1,'Connan',444,444,1,True,s,False)
                 gc.collect()
-                #print(s,total_reward,game.cave)
+                print(s,total_reward,game.cave)
                 done = True
 
         if steps%n_step == 0:
