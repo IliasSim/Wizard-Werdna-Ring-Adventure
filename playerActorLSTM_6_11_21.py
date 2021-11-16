98fbb9bdded36854ee93664bb3ebaca065a2504f
IliasSim <iliassimky@gmail.com>
2021-11-16 19:11:14 +0200

    Update playerActorLSTM_6_11_21.py

--- playerActorLSTM_6_11_21.py
+++ playerActorLSTM_6_11_21.py
@@ -30,6 +30,7 @@ dropoutRate2 = 0.3
 h_step = 8
 n_step = 4
 screenfactor = 1
+decay_steps = 10000
 input = (148,148,3)
 physical_devices = tf.config.list_physical_devices('GPU')
 try:
@@ -127,11 +128,12 @@ class actor(tf.keras.Model):
         return a
 
 class agent():
-    def __init__(self, gamma = 0.99):
+    def __init__(self,initial_learning_rate, gamma = 0.99):
         self.gamma = gamma
+        self.initial_learning_rate = initial_learning_rate
         lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(
-        initial_learning_rate=1e-5,
-        decay_steps=10000,
+        initial_learning_rate=self.initial_learning_rate,
+        decay_steps=decay_steps,
         decay_rate=0.9)
         self.a_opt = tf.keras.optimizers.Adam(lr_schedule)
         self.c_opt = tf.keras.optimizers.Adam(lr_schedule)
@@ -230,7 +232,12 @@ class agent():
         return a_loss, c_loss
 
 #tf.random.set_seed(336699)
-agentoo7 = agent()
+if exists('D:\ekpa\diplomatiki\Wizard-Werdna-Ring-Adventure\model_LSTM_6_11_21\steps.txt'):
+    f = open('D:\ekpa\diplomatiki\Wizard-Werdna-Ring-Adventure\model_LSTM_6_11_21\steps.txt','r')
+    total_steps = int(f.read())
+    f.close()
+initial_learning_rate = 1e-5*0.9**(total_steps/(h_step*decay_steps))
+agentoo7 = agent(initial_learning_rate)
 f = open('D:\ekpa\diplomatiki\Wizard-Werdna-Ring-Adventure\model_LSTM_6_11_21\episodes.txt','r')
 episodes_text = int(f.read())
 f.close()
@@ -242,13 +249,14 @@ if exists('D:\ekpa\diplomatiki\Wizard-Werdna-Ring-Adventure\model_LSTM_6_11_21\ 
     print("critic model is loaded")
     agentoo7.critic.built = True
     agentoo7.critic.load_weights('D:\ekpa\diplomatiki\Wizard-Werdna-Ring-Adventure\model_LSTM_6_11_21\ critic_model')
-episode = 10000
+episode = 10000 - episodes_text
 ep_reward = []
 total_avgr = []
 dfrewards = []
-game = GamePAI(1,'Connan',444,444,screenfactor,True,0,False)
+game = GamePAI(1,'Connan',444,444,screenfactor,True,episodes_text,False)
+game_No = episodes_text
 for s in range(episode):
-    game_No = s + 1
+    game_No = game_No + 1
     done = False
     state,playerStatus, gameText = game.initialGameState()
     #gameText = agentoo7.preprocess(gameText)
@@ -329,11 +337,12 @@ for s in range(episode):
             playerstatus = []
             gameTexts = []
             actions = []
-            rewards = []
+            # rewards = []
             # print(f"al{al}") 
             # print(f"cl{cl}")
     
         if done:
+            total_steps = total_steps + steps
             if s%100 == 0 and s != 0:
                 agentoo7.actor.save_weights('.\model_LSTM_6_11_21\ actor_model')
                 agentoo7.critic.save_weights('.\model_LSTM_6_11_21\ critic_model')
@@ -343,6 +352,10 @@ for s in range(episode):
                 f = open('D:\ekpa\diplomatiki\Wizard-Werdna-Ring-Adventure\model_LSTM_6_11_21\episodes.txt','r')
                 episodes_text = int(f.read())
                 f.close()
+                f = open('D:\ekpa\diplomatiki\Wizard-Werdna-Ring-Adventure\model_LSTM_6_11_21\steps.txt','w')
+                f.write(str(total_steps))
+                f.close()
+            print(total_steps,agentoo7.a_opt._decayed_lr(tf.float32))
             ep_reward.append(total_reward)
             avg_reward = np.mean(ep_reward[-100:])
             total_avgr.append(avg_reward)
