46e6d0a62ef91f103eb3134ad886d4732df38782
IliasSim <iliassimky@gmail.com>
2021-12-20 21:41:30 +0200

    Update playerActorLSTM_Imp_15_12_21.py

--- playerActorLSTM_Imp_15_12_21.py
+++ playerActorLSTM_Imp_15_12_21.py
@@ -18,7 +18,8 @@ CNN1Step = 1
 featuresCNN2 = 32
 CNN2Shape = 5
 CNN2Step = 2
-featuresCNN3 = 64
+# maxpooling
+featuresCNN3 = 16
 CNN3Shape = 10
 CNN3Step = 5
 denseLayerN = 256
@@ -29,7 +30,7 @@ denseLayerNL_31 = 128
 h_step = 8
 n_step = 4
 screenfactor = 1
-decay_steps = 40000
+decay_steps = 10000
 seeded = True
 input = (148,148,3)
 record = False
@@ -44,12 +45,12 @@ class critic(tf.keras.Model):
         super().__init__()
         self.l1 = tf.keras.layers.Conv3D(featuresCNN1,(1,CNN1Shape,CNN1Shape),(1,CNN1Step,CNN1Step),activation = 'relu')
         self.l2 = tf.keras.layers.Conv3D(featuresCNN2,(1,CNN2Shape,CNN2Shape),(1,CNN2Step,CNN2Step),activation = 'relu')
-        self.l3 = tf.keras.layers.Conv3D(featuresCNN3,(1,CNN3Shape,CNN3Shape),(1,CNN3Step,CNN3Step),activation = 'relu')
-        self.l4 = tf.keras.layers.Reshape((-1,10816))
+        # self.l3 = tf.keras.layers.Conv3D(featuresCNN3,(1,CNN3Shape,CNN3Shape),(1,CNN3Step,CNN3Step),activation = 'relu')
+        self.l4 = tf.keras.layers.Reshape((-1,161312))
         self.l5 = tf.keras.layers.LSTM(denseLayerN)
-        self.l6 = tf.keras.layers.Dense(denseLayerNL_2,activation = 'relu')
+        # self.l6 = tf.keras.layers.Dense(denseLayerNL_2,activation = 'relu')
         self.l7 = tf.keras.layers.LSTM(denseLayerNL_21)
-        self.l8 = tf.keras.layers.Dense(denseLayerNL_3,activation = 'relu')
+        # self.l8 = tf.keras.layers.Dense(denseLayerNL_3,activation = 'relu')
         self.l9 = tf.keras.layers.LSTM(denseLayerNL_31)
         self.conc1 = tf.keras.layers.Concatenate(axis=-1)
         self.conc2 = tf.keras.layers.Concatenate(axis=-1)
@@ -59,13 +60,13 @@ class critic(tf.keras.Model):
     def call(self,input_data,input_data1,input_data2):
         x = self.l1(input_data)
         x = self.l2(x)
-        x = self.l3(x)
+        # x = self.l3(x)
         x = self.l4(x)
         x= self.l5(x)
-        y = self.l6(input_data1)
-        y = self.l7(y)
-        z = self.l8(input_data2)
-        z = self.l9(z)
+        # y = self.l6(input_data1)
+        y = self.l7(input_data1)
+        # z = self.l8(input_data2)
+        z = self.l9(input_data2)
         h = self.conc1((y,z))
         g = self.conc2((h,x))
         v = self.v(g)
@@ -77,12 +78,12 @@ class actor(tf.keras.Model):
         super().__init__()
         self.l1 = tf.keras.layers.Conv3D(featuresCNN1,(1,CNN1Shape,CNN1Shape),(1,CNN1Step,CNN1Step),activation = 'relu')
         self.l2 = tf.keras.layers.Conv3D(featuresCNN2,(1,CNN2Shape,CNN2Shape),(1,CNN2Step,CNN2Step),activation = 'relu')
-        self.l3 = tf.keras.layers.Conv3D(featuresCNN3,(1,CNN3Shape,CNN3Shape),(1,CNN3Step,CNN3Step),activation = 'relu')
-        self.l4 = tf.keras.layers.Reshape((-1,10816))
+        # self.l3 = tf.keras.layers.Conv3D(featuresCNN3,(1,CNN3Shape,CNN3Shape),(1,CNN3Step,CNN3Step),activation = 'relu')
+        self.l4 = tf.keras.layers.Reshape((-1,161312))
         self.l5 = tf.keras.layers.LSTM(denseLayerN)
-        self.l6 = tf.keras.layers.Dense(denseLayerNL_2,activation = 'relu')
+        # self.l6 = tf.keras.layers.Dense(denseLayerNL_2,activation = 'relu')
         self.l7 = tf.keras.layers.LSTM(denseLayerNL_21)
-        self.l8 = tf.keras.layers.Dense(denseLayerNL_3,activation = 'relu')
+        # self.l8 = tf.keras.layers.Dense(denseLayerNL_3,activation = 'relu')
         self.l9 = tf.keras.layers.LSTM(denseLayerNL_31)
         self.conc1 = tf.keras.layers.Concatenate(axis=-1)
         self.conc2 = tf.keras.layers.Concatenate(axis=-1)
@@ -92,14 +93,13 @@ class actor(tf.keras.Model):
     def call(self,input_data,input_data1,input_data2):
         x = self.l1(input_data)
         x = self.l2(x)
-        x= self.l3(x)
+        # x= self.l3(x)
         x = self.l4(x)
         x= self.l5(x)
-        y = self.l6(input_data1)
-        print(input_data1.shape)
-        y = self.l7(y)
-        z = self.l8(input_data2)
-        z = self.l9(z)
+        # y = self.l6(input_data1)
+        y = self.l7(input_data1)
+        # z = self.l8(input_data2)
+        z = self.l9(input_data2)
         h = self.conc1((y,z))
         g = self.conc2((h,x))
         a = self.a(g)
@@ -118,36 +118,58 @@ class agent():
         self.actor = actor()
         self.critic = critic()
         self.log_prob = None
-        self.states = []
-        self.rewards = []
-        self.states = []
-        self.playerstatus = []
-        self.gameTexts = []
-        self.actions = []
-    
+        self.buffer_State = []
+        self.buffer_playerStatus = []
+        self.buffer_text = []
+        self.buffer_reward = []
+        self.buffer_size = 7
+
     def act(self,state,playerstatus,gameText):
         prob = self.actor(state,playerstatus,gameText)
-        # print(prob)
+        print(prob)
         prob = prob.numpy()
         dist = tfp.distributions.Categorical(probs=prob, dtype=tf.float32)
         action = dist.sample()
         return int(action.numpy()[0])
 
+    def preprocess0(self, state,playerstatus,gameText):
+        if len(self.buffer_State) > self.buffer_size:
+            del self.buffer_State[0]
+        self.buffer_State.append(state)
+        state = np.array(self.buffer_State)
+        state = np.expand_dims(state, axis=0)
 
-    def preprocess(self, actions, rewards, gamma):
+        if len(self.buffer_playerStatus) > self.buffer_size:
+            del self.buffer_playerStatus[0]
+        self.buffer_playerStatus.append(playerstatus)
+        playerstatus = np.array(self.buffer_playerStatus, dtype=np.float32)
+        playerstatus = np.expand_dims(playerstatus, axis=0)
+
+        if len(self.buffer_text) > self.buffer_size:
+            del self.buffer_text[0]
+        self.buffer_text.append(gameText)
+        gameText = np.array(self.buffer_text, dtype=np.float32)
+        gameText = np.expand_dims(gameText, axis=0)    
+        # print(state.shape,playerstatus.shape)
+        return state,playerstatus,gameText
+
+    def preprocess1(self, state,playerstatus,gameText, rewards, gamma):
         discnt_rewards = []
         sum_reward = 0        
         rewards.reverse()
         for r in rewards:
             sum_reward = r + gamma*sum_reward
             discnt_rewards.append(sum_reward)
+        rewards.reverse()
         discnt_rewards.reverse()
-        array_actions = np.array(actions)
-        array_actions = np.expand_dims(array_actions, axis=0)
-        discnt_rewards = np.array(discnt_rewards, dtype=np.float32)
-        discnt_rewards = np.expand_dims(discnt_rewards, axis=0)
-        print(array_actions.shape,discnt_rewards.shape)
-        return array_actions, discnt_rewards
+        discnt_rewards = discnt_rewards[-n_step:]
+        state = np.array(state)
+        state =  np.squeeze(state,axis = 1)
+        playerstatus = np.array(playerstatus)
+        playerstatus =  np.squeeze(playerstatus,axis = 1)
+        gameText = np.array(gameText)
+        gameText =  np.squeeze(gameText,axis = 1)
+        return  state,playerstatus,gameText,discnt_rewards
 
     def actor_loss(self, probs, actions, td): 
         probability = []
@@ -176,9 +198,7 @@ class agent():
         return loss
 
     def learn(self, states,playerstatus,gameTexts, actions, discnt_rewards):
-        # Learn function is called four seperate times, for each observation. For this reason changes are necessary. 
-        # Actions and probs are considered as unique instance so iterability is not necessary.
-        discnt_rewards = tf.reshape(discnt_rewards, (1,))
+        discnt_rewards = tf.reshape(discnt_rewards, (len(discnt_rewards),))
         # print(states.shape,playerstatus.shape,gameTexts.shape)
         with tf.GradientTape() as tape1, tf.GradientTape() as tape2:
             p = self.actor(states,playerstatus,gameTexts, training=True)
@@ -193,21 +213,23 @@ class agent():
         self.a_opt.apply_gradients(zip(grads1, self.actor.trainable_variables))
         self.c_opt.apply_gradients(zip(grads2, self.critic.trainable_variables))
         
-        if record and steps%8 == 0:
-            rows = 4
-            columns = 2
-            fig = plt.figure(figsize=(8, 8))
+        if record:
+            rows = 2
+            columns = 4
+            
             # print(states[0].shape[0])
-            for i in range(states[0].shape[0]):
-                print(i)
-                fig.add_subplot(rows, columns, i+1)
-                # print("LSTM " + str(i + 1) + str(playerstatus[i]))
-                print(states[0][i].shape)
-                plt.imshow(states[0][i])
-                plt.axis('off')
-                plt.title(action_name[actions])
-                print('end')
-            plt.show()
+            for i in range(n_step):
+                fig = plt.figure(figsize=(7, 7))
+                print(states.shape)
+                for z in range(h_step):
+                    # print(z)
+                    fig.add_subplot(rows, columns, z+1)
+                    print("LSTM " + str(i + 1) + str(playerstatus[i]))
+                    #print(states[i][z].shape)
+                    plt.imshow(states[i][z])
+                    plt.axis('off')
+                    # plt.title(action_name[actions[i]])
+                plt.show()
         return a_loss, c_loss
 
 if exists('D:\ekpa\diplomatiki\Wizard-Werdna-Ring-Adventure\playerActorLSTM_Imp_15_12_21\steps.txt'):
@@ -243,7 +265,14 @@ for s in range(episode):
     game_No = game_No + 1
     done = False
     state,playerStatus, gameText = game.initialGameState()
+    # print(state.shape)
+    state,playerStatus, gameText = agentoo7.preprocess0(state,playerStatus, gameText)
     total_reward = 0
+    rewards = []
+    train_actions = []
+    train_states = []
+    train_playerstatus = []
+    train_gametexts = []
     all_aloss = []
     all_closs = []
     steps = 0
@@ -260,18 +289,28 @@ for s in range(episode):
                     print('The record stops')
                     record = False
         steps += 1
-        # print(steps)
         action = agentoo7.act(state,playerStatus,gameText)
         next_state,reward, next_playerStatus, next_gameText,done  = game.playerAction(action)
         action_name = {0:'up',1:'right',2:'down',3:'left',4:'rest',5:'hp',6:'mp',7:'attack',8:'pick'}
-        # print(action_name[action],reward,game.cave)
-        if done:
-            game.__init__(1,'Connan',444,444,screenfactor,True,game_No,False,seeded)
-        states,playerstatus,gameTexts,rewards,actions = agentoo7.preprocess0(state,playerStatus,gameText,reward,action)
+        # print(reward)
+        print(action_name[action],reward,game.cave)
+        next_state,next_playerStatus,next_gameText = agentoo7.preprocess0(next_state,next_playerStatus,next_gameText)
         state = next_state
         playerStatus = next_playerStatus
         gameText = next_gameText
+        rewards.append(reward)
         total_reward += reward
+        if len(train_playerstatus) >= n_step:
+            train_actions = []
+            train_states = []
+            train_playerstatus = []
+            train_gametexts = []
+        train_actions.append(action)
+        train_states.append(state)
+        train_playerstatus.append(playerStatus)
+        train_gametexts.append(gameText)
+        if done:
+            game.__init__(1,'Connan',444,444,screenfactor,True,game_No,False,seeded)
 
         if steps%1000 == 0:
             print(steps,total_reward,action_name[action],game.cave)
@@ -298,24 +337,26 @@ for s in range(episode):
                 gc.collect()
                 print(s,total_reward,game.cave)
                 done = True
-
-
-        if len(states) == h_step:
+        if steps > h_step:
             if steps%n_step == 0:
-                actions, discnt_rewards = agentoo7.preprocess2(actions, rewards, 0.99)
-                for i in range(n_step,h_step):
+                train_states,train_playerstatus,train_gametexts,discnt_rewards= agentoo7.preprocess1(train_states,train_playerstatus,train_gametexts, rewards, 0.99)
+                al,cl = agentoo7.learn(train_states,train_playerstatus,train_gametexts, train_actions, discnt_rewards)
+                # print(train_state[0].shape)
+                # actions, discnt_rewards = agentoo7.preprocess2(actions, rewards, 0.99)
+                # for i in range(n_step,h_step):
                     # print(states[i].shape,playerstatus[i].shape,gameTexts[i].shape)
-                    al,cl = agentoo7.learn(states[i],playerstatus[i],gameTexts[i], actions[i], discnt_rewards[i])
+                    # al,cl = agentoo7.learn(states[i],playerstatus[i],gameTexts[i], actions[i], discnt_rewards[i])
                     # print("epoch " + str(i) +" " +  str(steps) + str(agentoo7.a_opt._decayed_lr(tf.float32)))
-                states = states[n_step:]
-                print(states[0].shape)
-                playerstatus = playerstatus[n_step:]
-                gameTexts = gameTexts[n_step:]
-                actions = actions[n_step:]
-                actions = actions.tolist()
+                # states = states[n_step:]
+                # print(states[0].shape)
+                # playerstatus = playerstatus[n_step:]
+                # gameTexts = gameTexts[n_step:]
+                # actions = actions[n_step:]
+                # actions = actions.tolist()
         
         if done:
             total_steps = total_steps + steps
+            agentoo7.last_discnt_rewards = 0
             if s%100 == 0 and s != 0:
                 agentoo7.actor.save_weights('.\playerActorLSTM_Imp_15_12_21\ actor_model')
                 agentoo7.critic.save_weights('.\playerActorLSTM_Imp_15_12_21\ critic_model')
